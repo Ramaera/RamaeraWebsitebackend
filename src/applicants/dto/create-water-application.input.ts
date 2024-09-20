bbdee67a867abcfd35811d79833942703404a0f5
Owais <owais.it@ramaera.in>
2024-09-20 16:13:11 +0530

    add more field  Water Applicant

--- src/applicants/dto/create-water-application.input.ts
+++ src/applicants/dto/create-water-application.input.ts
@@ -6,26 +6,56 @@ export class CreateWaterApplicantInput {
   name: string;
 
   @Field(() => String, { nullable: true })
-  mobile: string;
-
-  @Field(() => String, { nullable: true })
   email: string;
 
   @Field(() => String, { nullable: true })
-  pwID: string;
+  mobile: string;
 
   @Field(() => String, { nullable: true })
-  address: string;
+  altMobile: string;
 
   @Field(() => String, { nullable: true })
-  district: string;
+  dob: string;
+
+  @Field(() => String, { nullable: true })
+  aadhaar: string;
 
   @Field(() => String, { nullable: true })
   state: string;
 
   @Field(() => String, { nullable: true })
-  appliedFor: string;
+  district: string;
+
+  @Field(() => String, { nullable: true })
+  pincode: string;
+
+  @Field(() => String, { nullable: true })
+  address: string;
+
+  @Field(() => String, { nullable: true })
+  pwID: string;
+
+  @Field(() => String, { nullable: true })
+  firmName: string;
+
+  @Field(() => String, { nullable: true })
+  firmRegistration: string;
+
+  @Field(() => String, { nullable: true })
+  bankName: string;
+
+  @Field(() => String, { nullable: true })
+  bankAccount: string;
+
+  @Field(() => String, { nullable: true })
+  bankIfsc: string;
 
   @Field(() => String, { nullable: true })
   gst: string;
+
+  @Field(() => String, { nullable: true })
+  appliedFor: string;
+
+  @Field(() => String, { nullable: true })
+  distributorArea: string;
 }
