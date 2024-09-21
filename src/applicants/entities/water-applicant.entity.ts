4f849d65446a0146ba69e1758a32d6ff90243f84
Owais <owais.it@ramaera.in>
2024-09-21 11:53:10 +0530

    add field

--- src/applicants/entities/water-applicant.entity.ts
+++ src/applicants/entities/water-applicant.entity.ts
@@ -6,23 +6,56 @@ export class WaterApplicant {
   name: string;
 
   @Field(() => String, { nullable: true })
-  mobile: string;
-
-  @Field(() => String, { nullable: true })
   email: string;
 
   @Field(() => String, { nullable: true })
-  pwID: string;
+  mobile: string;
 
   @Field(() => String, { nullable: true })
-  address: string;
+  altMobile: string;
+
+  @Field(() => String, { nullable: true })
+  dob: string;
+
+  @Field(() => String, { nullable: true })
+  aadhaar: string;
+
+  @Field(() => String, { nullable: true })
+  state: string;
 
   @Field(() => String, { nullable: true })
   district: string;
 
   @Field(() => String, { nullable: true })
-  State: string;
+  pincode: string;
+
+  @Field(() => String, { nullable: true })
+  address: string;
+
+  @Field(() => String, { nullable: true })
+  pwID: string;
+
+  @Field(() => String, { nullable: true })
+  firmName: string;
+
+  @Field(() => String, { nullable: true })
+  firmRegistration: string;
+
+  @Field(() => String, { nullable: true })
+  bankName: string;
+
+  @Field(() => String, { nullable: true })
+  bankAccount: string;
+
+  @Field(() => String, { nullable: true })
+  bankIfsc: string;
+
+  @Field(() => String, { nullable: true })
+  gst: string;
 
   @Field(() => String, { nullable: true })
   appliedFor: string;
+
+  @Field(() => String, { nullable: true })
+  distributorArea: string;
 }
